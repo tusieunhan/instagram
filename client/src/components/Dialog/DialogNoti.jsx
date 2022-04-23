8bedcc5371cf4b7a996910e17a332b7bf5fbac0c
tusieunhan <levantu.iuh@gmail.com>
2022-04-23 17:28:52 +0700

    create post

--- client/src/components/Dialog/DialogNoti.jsx
+++ client/src/components/Dialog/DialogNoti.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import DialogNotiPost from './DialogNotiPost'
-import DialogNotiFollow from './DialogNotiFollow'
+import React from "react";
+import { useSelector } from "react-redux";
+import DialogNotiPost from "./DialogNotiPost";
+import DialogNotiFollow from "./DialogNotiFollow";
 
 const DialogNoti = () => {
-const tabFollow = useSelector((state) => state.box.tabFollow)
+  const tabFollow = useSelector((state) => state.box.tabFollow);
   return (
     <div className="dialog-noti">
-      {tabFollow  ?  <DialogNotiFollow/> : < DialogNotiPost/> }
+      {tabFollow ? <DialogNotiFollow /> : <DialogNotiPost />}
     </div>
-  )
-}
+  );
+};
 
-export default DialogNoti
+export default DialogNoti;
--- client/src/components/Dialog/DialogUpload.jsx
+++ client/src/components/Dialog/DialogUpload.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const DialogUpload = () => {
-  return (
-    <div className='dialog-upload' >
-        <div className="dialog-upload-header border-bottom">
-            <p>Create new post</p>
-        </div>
-        <div className="dialog-upload-body">
-            <i class="fa-thin fa-icons"></i>
-            <p>Drag photos and videos here</p>
-            <input type="file" multiple />
-        </div>
-    </div>
-  )
-}
+  const [avatar, setAvatar] = useState();
+  const [next, setNext] = useState(false);
+  const changeInputFile = (file) => {
+    const url = file.target.files[0];
+    let avt = URL.createObjectURL(url);
+    setAvatar(avt);
+  };
 
-export default DialogUpload
+  const handleClickInputFile = () => {
+    let inputClick = document.querySelector(".uploadfile");
+    inputClick.click();
+  };
+
+  return (
+    <div className="dialog-upload">
+      <div className="dialog-upload-header border-bottom flex-between">
+        <p>{avatar ? <i class="fa-light fa-arrow-left"></i> : " "}</p>
+        <p className="colortext">{avatar ? "Edit" : "Create new post"}</p>
+        <p className="colorlink">{avatar ? "Next" : " "}</p>
+      </div>
+      {avatar ? (
+        <img className="dialog-upload-img" src={avatar} />
+      ) : (
+        <div className="dialog-upload-body">
+          <i className="fa-thin fa-icons"></i>
+          <p>Drag photos and videos here</p>
+          <div className="btn-input" onClick={handleClickInputFile}>
+            <p>Select from computer</p>
+          </div>
+          <input
+            type="file"
+            multiple
+            name="uploadfile"
+            className="uploadfile"
+            onChange={changeInputFile}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DialogUpload;
