19abd97a9d523c34f63831cf52d08e59b15aeb8a
tusieunhan <levantu.iuh@gmail.com>
2022-04-23 18:30:21 +0700

    sussecced new post UI

--- client/src/components/Dialog/DialogUpload.jsx
+++ client/src/components/Dialog/DialogUpload.jsx
@@ -15,31 +15,60 @@ const DialogUpload = () => {
   };
 
   return (
-    <div className="dialog-upload">
-      <div className="dialog-upload-header border-bottom flex-between">
-        <p>{avatar ? <i class="fa-light fa-arrow-left"></i> : " "}</p>
-        <p className="colortext">{avatar ? "Edit" : "Create new post"}</p>
-        <p className="colorlink">{avatar ? "Next" : " "}</p>
-      </div>
-      {avatar ? (
-        <img className="dialog-upload-img" src={avatar} />
-      ) : (
-        <div className="dialog-upload-body">
-          <i className="fa-thin fa-icons"></i>
-          <p>Drag photos and videos here</p>
-          <div className="btn-input" onClick={handleClickInputFile}>
-            <p>Select from computer</p>
+    <>
+      {!next ? (
+        <div className="dialog-upload">
+          <div className="dialog-upload-header border-bottom flex-between">
+            <p onClick={() => setAvatar("")}>
+              {avatar ? <i className="fa-light fa-arrow-left"></i> : " "}
+            </p>
+            <p className="colortext">Create new post</p>
+            <p onClick={() => setNext(true)} className="colorlink">
+              {avatar ? "Next" : " "}
+            </p>
           </div>
-          <input
-            type="file"
-            multiple
-            name="uploadfile"
-            className="uploadfile"
-            onChange={changeInputFile}
-          />
+          {avatar ? (
+            <img className="dialog-upload-img" src={avatar} />
+          ) : (
+            <div className="dialog-upload-body ">
+              <i className="fa-thin fa-icons"></i>
+              <p>Drag photos and videos here</p>
+              <div className="btn-input" onClick={handleClickInputFile}>
+                <p>Select from computer</p>
+              </div>
+              <input
+                type="file"
+                multiple
+                name="uploadfile"
+                className="uploadfile"
+                onChange={changeInputFile}
+              />
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="dialog-upload-edit ">
+          <div className="dialog-upload-header border-bottom flex-between">
+            <p onClick={() => setNext(false)}>
+              {avatar ? <i class="fa-light fa-arrow-left"></i> : " "}
+            </p>
+            <p className="colortext">Create new post</p>
+            <p onClick={() => console.log("aaa")} className="colorlink">
+              {avatar ? "Next" : " "}
+            </p>
+          </div>
+          <div className="dialog-upload-edit-body flex">
+            <img className="dialog-upload-edit-img" src={avatar} />
+            <div className="dialog-upload-edit-newpost">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+              numquam suscipit illo impedit nobis maxime harum quam alias culpa
+              at consectetur nostrum modi expedita praesentium voluptatibus ea
+              placeat. Eos, a.
+            </div>
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
 
