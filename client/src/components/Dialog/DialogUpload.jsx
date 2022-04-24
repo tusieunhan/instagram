7504452d976d2957e2ad91b275b2a83e4b886f01
tusieunhan <levantu.iuh@gmail.com>
2022-04-24 16:31:41 +0700

    remake code commponent upload

--- client/src/components/Dialog/DialogUpload.jsx
+++ client/src/components/Dialog/DialogUpload.jsx
@@ -1,125 +1,113 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const DialogUpload = () => {
   const [avatar, setAvatar] = useState();
   const [currentAvatar, setCurrentAvatar] = useState();
   const [next, setNext] = useState(false);
-  const [index, setIndex] = useState(1)
+  const [index, setIndex] = useState(1);
   const changeInputFile = (file) => {
     let url = file.target.files;
-    if(url.length >= 1){
-      let arr = []
-      for (let i = 0; i < url.length; i++) {
-      let avt = URL.createObjectURL(url[i]);
-        arr.push(avt)
-      setAvatar(arr)
-      setCurrentAvatar(arr[index])
-      }
-    }else{
+    if (url.length === 1) {
       let avt = URL.createObjectURL(url[0]);
       setAvatar(avt);
+      setCurrentAvatar(avt);
+    } else {
+      let arr = [];
+      for (let i = 0; i < url.length; i++) {
+        let avt = URL.createObjectURL(url[i]);
+        arr.push(avt);
+        setAvatar(arr);
+        setCurrentAvatar(arr[index]);
+      }
     }
   };
-  
-  const nextAvt =()=>{
-    if(index === avatar.length - 1)  {
-      document.querySelector('.btn-next').classList.add('d-none')
-      return
-    } else{
-      document.querySelector('.btn-prev').classList.remove('d-none')
-    }
-    setIndex(i => i + 1)
-    console.log(index)
-  setCurrentAvatar(avatar[index])
-  }
-  const prevAvt =()=>{
-    if(index === 0) {
-      document.querySelector('.btn-prev').classList.add('d-none')
-      return
-    } else{
-      document.querySelector('.btn-next').classList.remove('d-none')
-    }
-    setIndex(i => i - 1)
-    console.log(index)
 
-  setCurrentAvatar(avatar[index])
-  }
+  console.log(avatar);
+  console.log(currentAvatar);
+
+  // const nextAvt = () => {
+  //   if (index === avatar.length - 1) {
+  //     document.querySelector(".btn-next").classList.add("d-none");
+  //     return;
+  //   } else {
+  //     document.querySelector(".btn-prev").classList.remove("d-none");
+  //   }
+  //   setIndex((i) => i + 1);
+  //   console.log(index);
+  //   setCurrentAvatar(avatar[index]);
+  // };
+  // const prevAvt = () => {
+  //   if (index === 0) {
+  //     document.querySelector(".btn-prev").classList.add("d-none");
+  //     return;
+  //   } else {
+  //     document.querySelector(".btn-next").classList.remove("d-none");
+  //   }
+  //   setIndex((i) => i - 1);
+  //   console.log(index);
+
+  //   setCurrentAvatar(avatar[index]);
+  // };
   const handleClickInputFile = () => {
     let inputClick = document.querySelector(".uploadfile");
     inputClick.click();
   };
   return (
     <>
-      {!next ? (
-        <div className="dialog-upload">
-          <div className="dialog-upload-header border-bottom flex-between">
-            <p onClick={() => setAvatar("")}>
-              {avatar ? <i className="fa-light fa-arrow-left"></i> : " "}
-            </p>
-            <p className="colortext">Create new post</p>
-            <p onClick={() => setNext(true)} className="colorlink">
-              {avatar ? "Next" : " "}
-            </p>
-          </div>
-          {avatar ? (
-           <>
-            <div onClick={prevAvt}  className=" btn-prev">
-            <i className="fa-regular fa-chevron-left"></i>
-            </div>
-            <img className="dialog-upload-img imgPreview" src={currentAvatar} />
-            <div onClick={nextAvt} className="btn-next">
-            <i className="fa-regular fa-chevron-right"></i>
-            </div>
-           </>
+      <div className="dialog-upload">
+        <div className="dialog-upload-header flex-between border-bottom">
+          {avatar ? <i class="fa-light fa-arrow-left"></i> : <p></p>}
+          <p>Create new post</p>
+          {avatar ? <p>Next</p> : <p></p>}
+        </div>
+        <div className="dialog-upload-body flex">
+          {avatar === undefined ? (
+            <div className="dialog-upload-preview">
+              <div className="dialog-upload-group flex">
+                <div className="dialog-upload-group-icon">
+                  <i className="fa-thin fa-icons"></i>
+                </div>
+                <div className="dialog-upload-group-title">
+                  Drag photos and videos here
+                </div>
+                <div
+                  onClick={handleClickInputFile}
+                  className="dialog-upload-group-btn_input btn-input"
+                >
+                  <p>Select from computer</p>
+                </div>
 
-          ) : (
-            <div className="dialog-upload-body ">
-              <i className="fa-thin fa-icons"></i>
-              <p>Drag photos and videos here</p>
-              <div className="btn-input" onClick={handleClickInputFile}>
-                <p>Select from computer</p>
+                <input
+                  onChange={changeInputFile}
+                  type="file"
+                  multiple
+                  name="uploadfile"
+                  className="uploadfile"
+                  style={{ display: "none" }}
+                />
               </div>
-              <input
-                type="file"
-                multiple
-                name="uploadfile"
-                className="uploadfile"
-                onChange={changeInputFile}
-              />
+            </div>
+          ) : (
+            <div className="dialog-upload-preview">
+              <div className="btn-next">
+                <i className="fa-light fa-chevron-right"></i>
+              </div>
+              <div className="btn-prev">
+                <i className="fa-light fa-chevron-left"></i>
+              </div>
+              <div className="dialog-upload-preview-img">
+                <img src={currentAvatar} alt="" />
+              </div>
             </div>
           )}
-        </div>
-      ) : (
-        <div className="dialog-upload-edit ">
-          <div className="dialog-upload-header border-bottom flex-between">
-            <p onClick={() => setNext(false)}>
-              
-              {avatar ? <i className="fa-light fa-arrow-left"></i> : " "}
-            </p>
-            <p className="colortext">Create new post</p>
-            <p onClick={() => console.log("aaa")} className="colorlink">
-              {avatar ? "Next" : " "}
-            </p>
-          </div>
-          <div className="dialog-upload-edit-body flex">
-            <div className="imgPreview">
-              <div onClick={prevAvt}  className=" btn-prev">
-              <i className="fa-regular fa-chevron-left"></i>
-              </div>
-              <img className="dialog-upload-edit-img" src={currentAvatar} />
-              <div onClick={nextAvt}  className=" btn-next">
-              <i className="fa-regular fa-chevron-right"></i>
-              </div>
-            </div>
-            <div className="dialog-upload-edit-newpost">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-              numquam suscipit illo impedit nobis maxime harum quam alias culpa
-              at consectetur nostrum modi expedita praesentium voluptatibus ea
-              placeat. Eos, a.
-            </div>
+          <div className="dialog-upload-edit">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ea
+            labore praesentium unde obcaecati modi suscipit blanditiis, error
+            velit explicabo hic exercitationem provident porro repellendus earum
+            expedita asperiores, optio corrupti.
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
