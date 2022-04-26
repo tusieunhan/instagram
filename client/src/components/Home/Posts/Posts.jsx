35ae12eea44ef9768635651d92db032739612ccf
tusieunhan <levantu.iuh@gmail.com>
2022-04-26 19:01:02 +0700

    add laibary Swiper use story

--- client/src/components/Home/Posts/Posts.jsx
+++ client/src/components/Home/Posts/Posts.jsx
@@ -1,16 +1,284 @@
-import React from 'react'
-
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Pagination } from "swiper";
+import "swiper/css";
 const Posts = () => {
   return (
     <div className="home-posts">
-        <div className="home-content">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, asperiores aliquid maxime perferendis id debitis eius molestiae excepturi, quibusdam beatae possimus officia aut quam dicta qui necessitatibus ipsum. Architecto, labore.
+      <div className="home-story">
+        <div className="home-story-list">
+          <Swiper
+            slidesPerView={7.5}
+            freeMode={true}
+            modules={[FreeMode, Pagination]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <div className="home-story-list-item">
+                <div className="home-story-list-item-img">
+                  <img
+                    src="https://topshare.vn/wp-content/uploads/2021/02/Hinh-soi-3D-sieu-dep-ngau-va-chat-nhat-1-edited.gif"
+                    alt=""
+                  />
+                </div>
+                <p>React js</p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="home-story-list-item">
+                <div className="home-story-list-item-img">
+                  <img
+                    src="https://topshare.vn/wp-content/uploads/2021/02/Hinh-soi-3D-sieu-dep-ngau-va-chat-nhat-1-edited.gif"
+                    alt=""
+                  />
+                </div>
+                <p>React js</p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="home-story-list-item">
+                <div className="home-story-list-item-img">
+                  <img
+                    src="https://topshare.vn/wp-content/uploads/2021/02/Hinh-soi-3D-sieu-dep-ngau-va-chat-nhat-1-edited.gif"
+                    alt=""
+                  />
+                </div>
+                <p>React js</p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="home-story-list-item">
+                <div className="home-story-list-item-img">
+                  <img
+                    src="https://topshare.vn/wp-content/uploads/2021/02/Hinh-soi-3D-sieu-dep-ngau-va-chat-nhat-1-edited.gif"
+                    alt=""
+                  />
+                </div>
+                <p>React js</p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="home-story-list-item">
+                <div className="home-story-list-item-img">
+                  <img
+                    src="https://topshare.vn/wp-content/uploads/2021/02/Hinh-soi-3D-sieu-dep-ngau-va-chat-nhat-1-edited.gif"
+                    alt=""
+                  />
+                </div>
+                <p>React js</p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="home-story-list-item">
+                <div className="home-story-list-item-img">
+                  <img
+                    src="https://topshare.vn/wp-content/uploads/2021/02/Hinh-soi-3D-sieu-dep-ngau-va-chat-nhat-1-edited.gif"
+                    alt=""
+                  />
+                </div>
+                <p>React js</p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="home-story-list-item">
+                <div className="home-story-list-item-img">
+                  <img
+                    src="https://topshare.vn/wp-content/uploads/2021/02/Hinh-soi-3D-sieu-dep-ngau-va-chat-nhat-1-edited.gif"
+                    alt=""
+                  />
+                </div>
+                <p>React js</p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="home-story-list-item">
+                <div className="home-story-list-item-img">
+                  <img
+                    src="https://topshare.vn/wp-content/uploads/2021/02/Hinh-soi-3D-sieu-dep-ngau-va-chat-nhat-1-edited.gif"
+                    alt=""
+                  />
+                </div>
+                <p>React js</p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="home-story-list-item">
+                <div className="home-story-list-item-img">
+                  <img
+                    src="https://topshare.vn/wp-content/uploads/2021/02/Hinh-soi-3D-sieu-dep-ngau-va-chat-nhat-1-edited.gif"
+                    alt=""
+                  />
+                </div>
+                <p>React js</p>
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
-        <div className="home-content">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque, asperiores aliquid maxime perferendis id debitis eius molestiae excepturi, quibusdam beatae possimus officia aut quam dicta qui necessitatibus ipsum. Architecto, labore.
-        </div>
-</div>
-  )
-}
+      </div>
+      <div className="home-content">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus
+        architecto dignissimos pariatur. Reiciendis, voluptatibus. Dolore soluta
+        blanditiis accusantium, asperiores eligendi incidunt maiores nisi?
+        Tenetur velit impedit beatae, nulla officiis eveniet! Voluptates
+        possimus neque nulla, mollitia accusamus eligendi pariatur, odit
+        voluptatem quo similique ratione recusandae vel! Corporis in cum laborum
+        quisquam sint magni quo quae magnam reprehenderit, temporibus eveniet
+        repudiandae dignissimos! Ab veritatis culpa eius perferendis, hic,
+        inventore voluptas harum molestiae et aperiam quidem laboriosam
+        blanditiis numquam? Ducimus, earum, numquam excepturi eum quod cum,
+        dolore quibusdam magni dolores ullam culpa autem? Similique sint nisi,
+        expedita a perspiciatis facilis, temporibus dolorem sed possimus atque
+        voluptatum aspernatur, quo sapiente distinctio eveniet! Velit iusto ad
+        nihil totam quas blanditiis assumenda expedita officiis, a laborum! Qui
+        corporis minima harum et quis esse architecto blanditiis. Optio quia
+        sunt eligendi ea odio earum quae recusandae cumque maxime, maiores
+        officiis sapiente. Fugiat itaque vel illo porro temporibus in. Aliquid
+        deleniti pariatur porro dolorum consectetur, repudiandae iure ut ipsum
+        explicabo odio consequatur veritatis recusandae quas natus distinctio
+        architecto iusto iste repellat? Esse debitis necessitatibus quis, fugit
+        blanditiis accusantium, asperiores eligendi incidunt maiores nisi?
+        Tenetur velit impedit beatae, nulla officiis eveniet! Voluptates
+        possimus neque nulla, mollitia accusamus eligendi pariatur, odit
+        voluptatem quo similique ratione recusandae vel! Corporis in cum laborum
+        quisquam sint magni quo quae magnam reprehenderit, temporibus eveniet
+        repudiandae dignissimos! Ab veritatis culpa eius perferendis, hic,
+        inventore voluptas harum molestiae et aperiam quidem laboriosam
+        blanditiis numquam? Ducimus, earum, numquam excepturi eum quod cum,
+        dolore quibusdam magni dolores ullam culpa autem? Similique sint nisi,
+        expedita a perspiciatis facilis, temporibus dolorem sed possimus atque
+        voluptatum aspernatur, quo sapiente distinctio eveniet! Velit iusto ad
+        nihil totam quas blanditiis assumenda expedita officiis, a laborum! Qui
+        corporis minima harum et quis esse architecto blanditiis. Optio quia
+        sunt eligendi ea odio earum quae recusandae cumque maxime, maiores
+        officiis sapiente. Fugiat itaque vel illo porro temporibus in. Aliquid
+        deleniti pariatur porro dolorum consectetur, repudiandae iure ut ipsum
+        explicabo odio consequatur veritatis recusandae quas natus distinctio
+        architecto iusto iste repellat? Esse debitis necessitatibus quis, fugit
+        blanditiis accusantium, asperiores eligendi incidunt maiores nisi?
+        Tenetur velit impedit beatae, nulla officiis eveniet! Voluptates
+        possimus neque nulla, mollitia accusamus eligendi pariatur, odit
+        voluptatem quo similique ratione recusandae vel! Corporis in cum laborum
+        quisquam sint magni quo quae magnam reprehenderit, temporibus eveniet
+        repudiandae dignissimos! Ab veritatis culpa eius perferendis, hic,
+        inventore voluptas harum molestiae et aperiam quidem laboriosam
+        blanditiis numquam? Ducimus, earum, numquam excepturi eum quod cum,
+        dolore quibusdam magni dolores ullam culpa autem? Similique sint nisi,
+        expedita a perspiciatis facilis, temporibus dolorem sed possimus atque
+        voluptatum aspernatur, quo sapiente distinctio eveniet! Velit iusto ad
+        nihil totam quas blanditiis assumenda expedita officiis, a laborum! Qui
+        corporis minima harum et quis esse architecto blanditiis. Optio quia
+        sunt eligendi ea odio earum quae recusandae cumque maxime, maiores
+        officiis sapiente. Fugiat itaque vel illo porro temporibus in. Aliquid
+        deleniti pariatur porro dolorum consectetur, repudiandae iure ut ipsum
+        explicabo odio consequatur veritatis recusandae quas natus distinctio
+        architecto iusto iste repellat? Esse debitis necessitatibus quis, fugit
+        blanditiis accusantium, asperiores eligendi incidunt maiores nisi?
+        Tenetur velit impedit beatae, nulla officiis eveniet! Voluptates
+        possimus neque nulla, mollitia accusamus eligendi pariatur, odit
+        voluptatem quo similique ratione recusandae vel! Corporis in cum laborum
+        quisquam sint magni quo quae magnam reprehenderit, temporibus eveniet
+        repudiandae dignissimos! Ab veritatis culpa eius perferendis, hic,
+        inventore voluptas harum molestiae et aperiam quidem laboriosam
+        blanditiis numquam? Ducimus, earum, numquam excepturi eum quod cum,
+        dolore quibusdam magni dolores ullam culpa autem? Similique sint nisi,
+        expedita a perspiciatis facilis, temporibus dolorem sed possimus atque
+        voluptatum aspernatur, quo sapiente distinctio eveniet! Velit iusto ad
+        nihil totam quas blanditiis assumenda expedita officiis, a laborum! Qui
+        corporis minima harum et quis esse architecto blanditiis. Optio quia
+        sunt eligendi ea odio earum quae recusandae cumque maxime, maiores
+        officiis sapiente. Fugiat itaque vel illo porro temporibus in. Aliquid
+        deleniti pariatur porro dolorum consectetur, repudiandae iure ut ipsum
+        explicabo odio consequatur veritatis recusandae quas natus distinctio
+        architecto iusto iste repellat? Esse debitis necessitatibus quis, fugit
+        blanditiis accusantium, asperiores eligendi incidunt maiores nisi?
+        Tenetur velit impedit beatae, nulla officiis eveniet! Voluptates
+        possimus neque nulla, mollitia accusamus eligendi pariatur, odit
+        voluptatem quo similique ratione recusandae vel! Corporis in cum laborum
+        quisquam sint magni quo quae magnam reprehenderit, temporibus eveniet
+        repudiandae dignissimos! Ab veritatis culpa eius perferendis, hic,
+        inventore voluptas harum molestiae et aperiam quidem laboriosam
+        blanditiis numquam? Ducimus, earum, numquam excepturi eum quod cum,
+        dolore quibusdam magni dolores ullam culpa autem? Similique sint nisi,
+        expedita a perspiciatis facilis, temporibus dolorem sed possimus atque
+        voluptatum aspernatur, quo sapiente distinctio eveniet! Velit iusto ad
+        nihil totam quas blanditiis assumenda expedita officiis, a laborum! Qui
+        corporis minima harum et quis esse architecto blanditiis. Optio quia
+        sunt eligendi ea odio earum quae recusandae cumque maxime, maiores
+        officiis sapiente. Fugiat itaque vel illo porro temporibus in. Aliquid
+        deleniti pariatur porro dolorum consectetur, repudiandae iure ut ipsum
+        explicabo odio consequatur veritatis recusandae quas natus distinctio
+        architecto iusto iste repellat? Esse debitis necessitatibus quis, fugit
+        blanditiis accusantium, asperiores eligendi incidunt maiores nisi?
+        Tenetur velit impedit beatae, nulla officiis eveniet! Voluptates
+        possimus neque nulla, mollitia accusamus eligendi pariatur, odit
+        voluptatem quo similique ratione recusandae vel! Corporis in cum laborum
+        quisquam sint magni quo quae magnam reprehenderit, temporibus eveniet
+        repudiandae dignissimos! Ab veritatis culpa eius perferendis, hic,
+        inventore voluptas harum molestiae et aperiam quidem laboriosam
+        blanditiis numquam? Ducimus, earum, numquam excepturi eum quod cum,
+        dolore quibusdam magni dolores ullam culpa autem? Similique sint nisi,
+        expedita a perspiciatis facilis, temporibus dolorem sed possimus atque
+        voluptatum aspernatur, quo sapiente distinctio eveniet! Velit iusto ad
+        nihil totam quas blanditiis assumenda expedita officiis, a laborum! Qui
+        corporis minima harum et quis esse architecto blanditiis. Optio quia
+        sunt eligendi ea odio earum quae recusandae cumque maxime, maiores
+        officiis sapiente. Fugiat itaque vel illo porro temporibus in. Aliquid
+        deleniti pariatur porro dolorum consectetur, repudiandae iure ut ipsum
+        explicabo odio consequatur veritatis recusandae quas natus distinctio
+        architecto iusto iste repellat? Esse debitis necessitatibus quis, fugit
+        blanditiis accusantium, asperiores eligendi incidunt maiores nisi?
+        Tenetur velit impedit beatae, nulla officiis eveniet! Voluptates
+        possimus neque nulla, mollitia accusamus eligendi pariatur, odit
+        voluptatem quo similique ratione recusandae vel! Corporis in cum laborum
+        quisquam sint magni quo quae magnam reprehenderit, temporibus eveniet
+        repudiandae dignissimos! Ab veritatis culpa eius perferendis, hic,
+        inventore voluptas harum molestiae et aperiam quidem laboriosam
+        blanditiis numquam? Ducimus, earum, numquam excepturi eum quod cum,
+        dolore quibusdam magni dolores ullam culpa autem? Similique sint nisi,
+        expedita a perspiciatis facilis, temporibus dolorem sed possimus atque
+        voluptatum aspernatur, quo sapiente distinctio eveniet! Velit iusto ad
+        nihil totam quas blanditiis assumenda expedita officiis, a laborum! Qui
+        corporis minima harum et quis esse architecto blanditiis. Optio quia
+        sunt eligendi ea odio earum quae recusandae cumque maxime, maiores
+        officiis sapiente. Fugiat itaque vel illo porro temporibus in. Aliquid
+        deleniti pariatur porro dolorum consectetur, repudiandae iure ut ipsum
+        explicabo odio consequatur veritatis recusandae quas natus distinctio
+        architecto iusto iste repellat? Esse debitis necessitatibus quis, fugit
+        blanditiis accusantium, asperiores eligendi incidunt maiores nisi?
+        Tenetur velit impedit beatae, nulla officiis eveniet! Voluptates
+        possimus neque nulla, mollitia accusamus eligendi pariatur, odit
+        voluptatem quo similique ratione recusandae vel! Corporis in cum laborum
+        quisquam sint magni quo quae magnam reprehenderit, temporibus eveniet
+        repudiandae dignissimos! Ab veritatis culpa eius perferendis, hic,
+        inventore voluptas harum molestiae et aperiam quidem laboriosam
+        blanditiis numquam? Ducimus, earum, numquam excepturi eum quod cum,
+        dolore quibusdam magni dolores ullam culpa autem? Similique sint nisi,
+        expedita a perspiciatis facilis, temporibus dolorem sed possimus atque
+        voluptatum aspernatur, quo sapiente distinctio eveniet! Velit iusto ad
+        nihil totam quas blanditiis assumenda expedita officiis, a laborum! Qui
+        corporis minima harum et quis esse architecto blanditiis. Optio quia
+        sunt eligendi ea odio earum quae recusandae cumque maxime, maiores
+        officiis sapiente. Fugiat itaque vel illo porro temporibus in. Aliquid
+        deleniti pariatur porro dolorum consectetur, repudiandae iure ut ipsum
+        explicabo odio consequatur veritatis recusandae quas natus distinctio
+        architecto iusto iste repellat? Esse debitis necessitatibus quis, fugit
+        harum molestiae neque. Possimus fugiat reprehenderit reiciendis
+        assumenda, quos vitae quas tempora itaque fuga enim. Beatae tenetur quod
+        adipisci atque facilis velit necessitatibus fuga nisi doloribus ipsa
+        inventore vel nostrum facere, asperiores et. Id inventore sequi veniam
+        voluptatum! Aperiam saepe rerum possimus in laboriosam? Laboriosam harum
+        sint quisquam velit ducimus obcaecati sed repudiandae odit doloribus
+        qui, cupiditate natus sit dicta doloremque reiciendis similique. Quam
+        quae laboriosam cumque esse tempore necessitatibus quasi animi omnis
+        consequatur vero adipisci iste, magnam ducimus odit libero! Modi, magni
+        vitae? Molestias odit illum ad labore nesciunt molestiae porro soluta.
+        Labore fugiat nesciunt voluptates quibusdam illum saepe dolorem,
+        voluptatum nemo, odit dignissimos consequatur? Sed, dolorem laborum
+        recusandae distinctio quae aliquam! Rerum reprehenderit soluta dicta
+        nobis quod, tenetur officia delectus quas!
+      </div>
+    </div>
+  );
+};
 
-export default Posts
+export default Posts;
