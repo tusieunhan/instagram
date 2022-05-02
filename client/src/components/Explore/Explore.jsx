7b0ff6faa746b131298367d3133ea3e8d35c60e6
tusieunhan <levantu.iuh@gmail.com>
2022-05-02 22:48:19 +0700

    layout explore

--- client/src/components/Explore/Explore.jsx
+++ client/src/components/Explore/Explore.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
-
+import React from "react";
+import "../../style/explore.scss";
 const Explore = () => {
-  return (
-    <div>Explore</div>
-  )
-}
+  //  1
+  //  2
+  //  3
+  //  -right
+  //  4
+  //  5
+  //  6
+  //  -none
+  //  7
+  //  8
+  //  9
+  //  -left
+  //  10
+  //  11
+  //  12
+  //  13
+  //  14
+  //  15
+  //  16
+  //  17
+  //  18
+  //  19
+  //  20
+  //  21
 
-export default Explore
+  return (
+    <div className="explore">
+      <div className="explore-list">
+        <div class="explore-list-item explore-item-left"></div>
+        <div class="explore-list-item"></div>
+        <div class="explore-list-item"></div>
+      </div>
+      <div className="explore-list">
+        <div class="explore-list-item"></div>
+        <div class="explore-list-item"></div>
+        <div class="explore-list-item"></div>
+      </div>
+      <div className="explore-list">
+        <div class="explore-list-item"></div>
+        <div class="explore-list-item"></div>
+        <div class="explore-list-item"></div>
+      </div>
+      <div className="explore-list">
+        <div class="explore-list-item explore-item-right"></div>
+        <div class="explore-list-item"></div>
+        <div class="explore-list-item"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
